6e703fc6f41da31b9da3660a50b1d4a1b6257a05
Vasu Goel <goelvasu0712@gmail.com>
2019-08-25 18:08:10 +0530

    For each sentiment, compute odds of being in the device

--- sentiment-analysis.R
+++ sentiment-analysis.R
@@ -5,6 +5,19 @@ library(dslabs)
 library(tidytext)
 
 # using nrc lexicon from tidytext for sentiment analysis
-nrc <- sentiments %>%
-  filter(lexicon == "nrc") %>%
-  select(word, sentiment)
+nrc <- get_sentiments("nrc")
+
+# count and compare the frequencies of each sentiment appearing in each device
+sentiment_counts <- tweet_words %>%
+  left_join(nrc, by = "word") %>%
+  count(source, sentiment) %>%
+  spread(source, n) %>%
+  mutate(sentiment = replace_na(sentiment, replace = "none"))
+sentiment_counts
+
+# for each sentiment, compute the odds of being in the device
+sentiment_counts %>%
+  mutate(Android = Android / (sum(Android) - Android) , 
+         iPhone = iPhone / (sum(iPhone) - iPhone), 
+         or = Android/iPhone) %>%
+  arrange(desc(or))
